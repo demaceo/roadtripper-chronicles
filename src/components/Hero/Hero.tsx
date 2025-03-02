fdb312eeec1b396425fcd57a5f4fd8c146a69280
demaceo <hdemaceo@gmail.com>
2025-03-02 14:02:48 -0700

    rename hero class attributes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,53 +1,5 @@
-// import React from "react";
-// import "./Hero.css";
-
-// const Hero: React.FC = () => {
-//   return (
-//     <div className="wrapper">
-//       <div
-//         className="page-header section-dark"
-//         style={{
-//           backgroundImage:
-//             "url('http://demos.creative-tim.com/paper-kit-2/assets/img/antoine-barres.jpg')",
-//         }}
-//       >
-//         <div className="filter"></div>
-//         <div className="content-center">
-//           <div className="container">
-//             <div className="title-brand">
-//               <h1 className="presentation-title">ROADTRIPPER CHRONICLES</h1>
-//               <div className="fog-low">
-//                 <img
-//                   src="http://demos.creative-tim.com/paper-kit-2/assets/img/fog-low.png"
-//                   alt="fog"
-//                 />
-//               </div>
-//               <div className="fog-low right">
-//                 <img
-//                   src="http://demos.creative-tim.com/paper-kit-2/assets/img/fog-low.png"
-//                   alt="fog"
-//                 />
-//               </div>
-//             </div>
-//             <h2 className="presentation-subtitle text-center">The Podcast</h2>
-//           </div>
-//         </div>
-//         <div
-//           className="moving-clouds"
-//           style={{
-//             backgroundImage:
-//               "url('http://demos.creative-tim.com/paper-kit-2/assets/img/clouds.png')",
-//           }}
-//         ></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
 import React from "react";
-import "./Hero.css"; // Import custom CSS
+import "./Hero.css"; 
 
 interface HeroProps {
   title: string;
@@ -65,16 +17,16 @@ const Hero: React.FC<HeroProps> = ({
   fogImage,
 }) => {
   return (
-    <div className="wrapper">
+    <div className="hero-wrapper">
       <div
-        className="page-header section-dark"
+        className="hero-background section-dark"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <div className="filter"></div>
+        <div className="overlay-filter"></div>
         <div className="content-center">
-          <div className="container">
+          <div className="hero-title-container">
             <div className="title-brand">
-              <h1 className="presentation-title">{title}</h1>
+              <h1 className="hero-title">{title}</h1>
               <div className="fog-low">
                 <img src={fogImage} alt="Fog" />
               </div>
@@ -83,7 +35,7 @@ const Hero: React.FC<HeroProps> = ({
               </div>
             </div>
 
-            <h2 className="presentation-subtitle text-center">{subtitle}</h2>
+            <h2 className="hero-subtitle">{subtitle}</h2>
           </div>
         </div>
         <div
